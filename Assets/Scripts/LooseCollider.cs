3bf11d0ca88ab359162219eba496baf5ebd3162a
Redzone <ub-epr@UB-DEV2.ukrborg.ua>
2018-01-23 17:26:34 +0200

    First Commit
    Csene

---
+++ Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseCollider : MonoBehaviour
+{
+
+    public LevelManager levelManager;
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger ");
+        levelManager.LoadLevel("Win");
+    }
+
+    void OnCollisionEnter2D(Collision2D colllision)
+    {
+        print("Collision");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
